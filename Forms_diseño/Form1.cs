802bc29ded7af380ee24933e10c091b42f2ad2fc
gastyft <gastyftacla1@hotmail.com>
2023-05-13 08:40:38 -0300

    Creacion de clases principales

---
+++ Forms_diseño/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Bienvenido a la App..");
+        }
+
+        private void linkedInToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VisitLinkLinkedln();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        private void VisitLinkLinkedln()
+        {
+          
+            System.Diagnostics.Process.Start("https://www.linkedin.com/in/gaston-manuel-ftacla-04b57b229/");
+            
+        }
+
+        private void instagramToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VisitLinkInstagram();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        private void VisitLinkInstagram()
+        {
+
+            System.Diagnostics.Process.Start("https://www.instagram.com/gastyftacla/");
+
+        }
+
+        private void miPortfolioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VisitLinkPortfolio();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+        }
+
+        private void VisitLinkPortfolio()
+        {
+
+            System.Diagnostics.Process.Start("https://portfolio-yo-programo.web.app/principal");
+
+        }
+
+        private void campusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                VisitLinkCampus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+
+
+        }
+
+        private void VisitLinkCampus()
+        {
+
+            System.Diagnostics.Process.Start("https://campusmaxiprograma.com/");
+
+        }
+
+        private void instagramToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VisitLinkInstagramMaxi();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+           
+        }
+        private void VisitLinkInstagramMaxi()
+        {
+
+            System.Diagnostics.Process.Start("https://www.instagram.com/maxiprograma/");
+
+        }
+
+        private void cursosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                VisitLinkCursosMaxi();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to open link that was clicked.");
+            }
+
+        }
+        private void VisitLinkCursosMaxi()
+        {
+
+            System.Diagnostics.Process.Start("https://maxiprograma.com/");
+
+        }
+
+    }
+    }
+    
+
+    
+
---
+++ dominio/Producto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using dominio;
+
+
+namespace Dominio
+{
+    public class Producto
+    {
+        public int Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public string Imagen { get; set; }
+        public string Precio { get; set; }
+
+        public Marca Marca { get; set; }
+
+        public Categoria Nombre_categoria { get; set; }
+
+        // crear clase Marca(seleccionable de una lista desplegable). e importar.
+        // crear clase Categoría(seleccionable de una lista desplegable. e importar.
+    }
+}
